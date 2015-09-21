18fe2e70d33fc920b1735aafc5f63563d8cd2970
Rodrigo Arano <>
2015-09-21 01:41:47 -0400

    Menu responsive pas mal fini (encore seulement sur movie.html)

---
+++ menu.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Rod on 21/9/15.
+ */
+
+function menuOnOff(){
+    if(document.getElementById("miniMenu").style.display == "block"){
+        document.getElementById("miniMenu").style.display = "none";
+    }
+    else{
+        document.getElementById("miniMenu").style.display = "block";
+    }
+}
