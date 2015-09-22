3835dd09d192fd4c52265a9d14c0cd0d985842e8
Rodrigo Arano <rodrigo.arano@me.com>
2015-09-22 02:14:52 -0400

    Menu fait OK en trois versions. Modèle à suivre voir movie.html.

--- menu.js
+++ menu.js
@@ -10,3 +10,14 @@ function menuOnOff(){
         document.getElementById("miniMenu").style.display = "block";
     }
 }
+
+function menuWatchlistOnOff(){
+    if(document.getElementById("myWatchlistMenu").style.display == "block"){
+        document.getElementById("myWatchlistMenu").style.display = "none";
+        document.getElementById("myWatchlist").style.backgroundColor = "rgba(20, 20, 20, 0)";
+    }
+    else{
+        document.getElementById("myWatchlistMenu").style.display = "block";
+        document.getElementById("myWatchlist").style.backgroundColor = "#16A79D";
+    }
+}
