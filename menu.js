1beee54cebea2ac411cd1a4f2762e9af1f082289
noelbetancourt <noel-alonso.betancourt-delgado.1@ulaval.ca>
2015-09-25 16:32:00 -0400

    index deuxième version incluant les  modification du fichier stylesheet.css

--- menu.js
+++ menu.js
@@ -5,9 +5,11 @@
 function menuOnOff(){
     if(document.getElementById("miniMenu").style.display == "block"){
         document.getElementById("miniMenu").style.display = "none";
+
     }
     else{
         document.getElementById("miniMenu").style.display = "block";
+
     }
 }
 
