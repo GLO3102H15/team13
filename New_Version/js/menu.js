f26ec5d04b89e61f7ee5c94b42cef9cb84e49f24
Rodrigo Arano <rodrigo.arano@me.com>
2015-11-03 10:36:24 -0500

    Nouvelle version pour le TP 2

---
+++ New_Version/js/menu.js
@@ -0,0 +1,25 @@
+/**
+ * Created by Rod on 21/9/15.
+ */
+
+function menuOnOff(){
+    if(document.getElementById("miniMenu").style.display == "block"){
+        document.getElementById("miniMenu").style.display = "none";
+
+    }
+    else{
+        document.getElementById("miniMenu").style.display = "block";
+
+    }
+}
+
+function menuWatchlistOnOff(){
+    if(document.getElementById("myWatchlistMenu").style.display == "block"){
+        document.getElementById("myWatchlistMenu").style.display = "none";
+        document.getElementById("myWatchlist").style.backgroundColor = "rgba(20, 20, 20, 0)";
+    }
+    else{
+        document.getElementById("myWatchlistMenu").style.display = "block";
+        document.getElementById("myWatchlist").style.backgroundColor = "#16A79D";
+    }
+}
