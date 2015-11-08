d203f4abfda2d9ea76daedf0947ad4bdbbbf26c7
tbnnrdt <thomas.bonnardot@hei.fr>
2015-11-08 11:15:07 -0500

    Fix ok pour movie

--- TP2_New/resources/js/script.js
+++ TP2_New/resources/js/script.js
@@ -1,10 +1,40 @@
 /**
  * Created by thomasbonnardot on 07/11/2015.
  */
+$(document).ready(function(){
+    $('body').delegate('#glyph-watchlist-present','click',function() {
+        $('#myModal-movie').modal('show');
+        $('#myModal-movie').css('display', 'block');
+    });
+    $('body').delegate('#go-watchlist','click',function(){
+        setTimeout(function(){ window.location.href = '#/watchlists'; }, 400);
+    });
+    $('body').delegate('#glyph-watchlist-ajouter','click',function(){
+        $('#myModal-watchlist').modal('show');
+        $('#myModal-watchlist').css('display','block');
+    });
+
+    $('body').delegate('#ajouter-movie-watchlist','click',function(){
+        id_watchlist = $('#myModal-watchlist select').val();
+        id_movie = $('#id_movie').val();
+        console.log('click sur Ajout Watchlist');
+        var film;
+        var watchlist;
+        $.get('http://localhost:3000/unsecure/movies/'+id_movie,function( data ) {
+            film = data.results[0];
+            $.post('http://localhost:3000/unsecure/watchlists/'+id_watchlist+'/movies',film,function( data ){
+                getWatchlist();
+            });
+        });
+
+    });
+});
+
 function getWatchlist(){
     console.log('fonction getWatchlist');
     $('#colInfo h3 .glyphicon').remove();
     $('#myModal-watchlist select option').remove();
+    $('#watchlist-deja-present ul li').remove();
     $.get('http://localhost:3000/unsecure/watchlists',function( data ) {
         $('#colInfo h3').append('<span class="glyphicon glyphicon-plus glyph-watchlist" id="glyph-watchlist-ajouter"></span>');
         id = $('#id_movie').val();
@@ -26,6 +56,7 @@ function getWatchlist(){
             }
             if (flag_present){
                 html += '<small>  (déjà présent)</small>';
+                $('#watchlist-deja-present ul').append('<li>' + data[i].name + '</li>')
             }
             html += '</option>';
             $('#myModal-watchlist select').append(html);
