6d9e1844bc470dfbaa01503dd80a9dfbcde53c65
tbnnrdt <thomas.bonnardot@hei.fr>
2015-12-12 20:15:23 -0500

    debut Follow

--- TP2_New/resources/js/views/userView.js
+++ TP2_New/resources/js/views/userView.js
@@ -5,7 +5,9 @@
     UserView = Backbone.View.extend({
         //template: _.template($('#tvshow-episodes-tpl').html()),
         el: '#show_content',
-        events: {},
+        events: {
+            'click .btn-follow': 'followUser'
+        },
         initialize: function(){
             _.bindAll(this, 'render');
             var self = this;
@@ -32,6 +34,28 @@
                 self.template = _.template(data);
                 self.$el.html(self.template({user : self.model.toJSON()}));
             }, 'html');
+        },
+        followUser: function(){
+            console.log('Click Follow');
+            var self = this;
+            var idFollow = self.model.toJSON().id;
+            $.ajax({
+                url: "http://localhost:3000/follow",
+                type: 'POST',
+                data: {id:idFollow.toString()},
+                dataType: 'JSON',
+                async: true,
+                success: function (data) {
+                    console.log('ok');
+                    console.log(data);
+                },
+                error: function (data) {
+                    if(data.status == 401){
+                        console.log("Token expired");
+                        app_router.navigate("",true);
+                    }
+                }
+            });
         }
     });
 })();
